9aa5a596a3afc1bfe5a82cd3009471f22e49397f
shahank42 <shounakghosh42@gmail.com>
2024-08-14 04:41:02 +0530

    chore: Remove unused ModeWatcher component from layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script>
 	import Navbar from '$lib/components/navbar.svelte';
-	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 </script>
 
-<ModeWatcher />
 <main class="relative flex min-h-screen flex-col">
 	<Navbar />
 	<div class="flex-1 flex-grow">
