27354d2a6d1e94e548d39847814384c09eabf862
shahank42 <shounakghosh42@gmail.com>
2024-08-15 20:11:50 +0530

    chore: Update cart page with empty cart message and link to home

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
 	import CartCard from '$lib/components/cart-card/cart-card.svelte';
 	import MaxWidthWrapper from '$lib/components/max-width-wrapper.svelte';
-	import Button from '$lib/components/ui/button/button.svelte';
-	import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '$lib/components/ui/card';
-	import { Input } from '$lib/components/ui/input';
-	import { Label } from '$lib/components/ui/label';
-	import { Separator } from '$lib/components/ui/separator';
 	import { cart } from '$lib/stores/cartStore';
-	import { formatPrice } from '$lib/utils';
-	import { ChevronRight } from 'lucide-svelte';
+	import { cn } from '$lib/utils';
 	import CartDetails from './cart-details.svelte';
+	import { buttonVariants } from '$lib/components/ui/button';
 </script>
 
 <section class="w-full bg-background py-8 text-foreground md:py-12">
 	<MaxWidthWrapper class="grid grid-cols-1 gap-8 md:grid-cols-[2fr_1fr]">
 		<div class="flex flex-col gap-6">
 			<h1 class="text-3xl font-semibold">My Cart</h1>
+			<p class="">
+				Your cart is empty! Looks like it's <a
+					href="/"
+					class={cn(buttonVariants({ variant: 'link' }), 'px-0')}>shopping time!</a
+				>
+			</p>
 			<div class="flex flex-col gap-3">
 				{#each $cart.items as item}
 					<CartCard {item} />
